883bb3e779982c0a4b519610574138441c3ddfc8
nelysalinas <nelysalinas5@fpuna.edu.py>
2021-09-04 14:43:51 -0400

    Formulario de Edit de Usuario con estilos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  devise :database_authenticatable, :rememberable, :validatable, :recoverable, :registerable
   
   enum role: { secretario: 0, logistica: 1, profesional: 2, administrador: 3 }
 end
